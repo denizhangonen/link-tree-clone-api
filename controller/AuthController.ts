7bdef3ec562ccc4d143c99f1db116fe167ca2eaf
Denizhan Gonen <gonendenizhan@gmail.com>
2024-02-11 21:53:20 +0300

    login implemented

--- controller/AuthController.ts
+++ controller/AuthController.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs'; // For password hashing
 import UserModel, { IUser } from '../model/User';
 
+const AUTH_TOOLS = require('../util/AUTH_TOOLS');
+
 export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
@@ -20,14 +22,22 @@ export const login = async (req: Request, res: Response) => {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
-    // Authentication successful
-    return res.status(200).json({ message: 'Authentication successful' });
+    const token = AUTH_TOOLS.generateToken(email, user._id);
+
+    // return response
+    res.status(200).json({
+      accessToken: token,
+      user: {
+        _id: user._id,
+        email: user.email,
+        fullName: user.fullName,
+      },
+    });
   } catch (error) {
     console.error('Error during login:', error);
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
-
 
 // Signup controller function
 export const signup = async (req: Request, res: Response) => {
@@ -39,7 +49,9 @@ export const signup = async (req: Request, res: Response) => {
     // Check if user already exists with the provided email
     const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'User with this email already exists' });
+      return res
+        .status(400)
+        .json({ message: 'User with this email already exists' });
     }
 
     // Hash the password
--- util/AUTH_TOOLS.ts
+++ util/AUTH_TOOLS.ts
@@ -1,17 +1,14 @@
 import jwt from 'jsonwebtoken';
 
-export const generateToken = (email: string, userId: string, role: string) => {
+export const generateToken = (email: string, userId: string) => {
   // Check if TOKEN_SECRET is defined
   if (!process.env.TOKEN_SECRET) {
     throw new Error('TOKEN_SECRET is not defined in environment variables');
   }
 
-  const token = jwt.sign(
-    { email, userId, role },
-    process.env.TOKEN_SECRET,
-    {
-      expiresIn: '9999h',
-    }
-  );
+  const token = jwt.sign({ email, userId }, process.env.TOKEN_SECRET, {
+    expiresIn: '9999h',
+  });
   return token;
 };
+
